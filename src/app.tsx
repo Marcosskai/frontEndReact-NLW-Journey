163bbdfb3c18ddf22797e40689eed07e6a637859
Marcos Gabriel <marcos.gabrielr150@gmail.com>
2024-07-30 21:23:00 -0300

    disabled isGhestsInputOpen

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
 import { useState } from 'react'
 
 export function App() {
@@ -20,20 +20,27 @@ export function App() {
 
       <div className='flex items-center gap-2 flex-1'>
            <MapPin className='size-5 text-zinc-400' />
-          <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+          <input disabled={isGhestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
       </div>
 
       <div className='flex items-center gap-2'>
         <Calendar className='size-5 text-zinc-400' />
-        <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
+        <input disabled={isGhestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
       </div>
  
       <div className='w-px h-6 bg-zinc-800' />
 
-    <button onClick={openGhestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-      Continuar
-      <ArrowRight className='size- 5' />
-    </button>
+      {isGhestsInputOpen ? (
+        <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+          Alterar Local/data
+          <Settings2 className='szie-5'/>
+          </button>
+      ): (
+        <button onClick={openGhestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+          Continuar
+          <ArrowRight className='size- 5' />
+        </button> 
+      )}
 
   </div>
 
@@ -42,14 +49,14 @@ export function App() {
          <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center gap-3">
 
          <div className='flex items-center gap-2 flex-1'>
-              <MapPin className='size-5 text-zinc-400' />
+              <UserRoundPlus className='size-5 text-zinc-400' />
              <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
          </div>
     
          <div className='w-px h-6 bg-zinc-800' />
    
-       <button onClick={openGhestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-         Continuar
+       <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+         Confirmar Viagem
          <ArrowRight className='size- 5' />
        </button>
    
