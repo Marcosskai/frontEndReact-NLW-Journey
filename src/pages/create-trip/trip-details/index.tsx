81ad5435b57c008ab40c658e2245a5cc2e1137b2
Marcos Gabriel <marcos.gabrielr150@gmail.com>
2024-08-07 20:40:43 -0300

    co

--- src/pages/create-trip/trip-details/index.tsx
+++ src/pages/create-trip/trip-details/index.tsx
@@ -64,67 +64,67 @@ export function TripDetailsPage () {
                       </div>
 
                     </div>
-                </div>
+            </div>
 
-                <div className="w-80 space-y-6">
+            <div className="w-80 space-y-6">
 
-                    <div className="space-y-6">
-                        <h2 className="font-semibold text-xl">Links Importantes</h2>
-                        <div className="space-y-5">
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5 flex-1">
-                                    <span className="block font-medium text-zinc-100">Reserva do AirBnb</span>
-                                    <a href='#' className="block text-sx text-zinc-400 truncate hover:text-zinc-200">
-                                        https://wwww.airbnb.com.br/rooms/10470001165465465465465132132
-                                    </a>
-                                </div>
-                                <Link2 className="text-zinc-400 size-5"/>
+                <div className="space-y-6">
+                    <h2 className="font-semibold text-xl">Links Importantes</h2>
+                    <div className="space-y-5">
+                        <div className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">Reserva do AirBnb</span>
+                                <a href='#' className="block text-sx text-zinc-400 truncate hover:text-zinc-200">
+                                    https://wwww.airbnb.com.br/rooms/10470001165465465465465132132
+                                </a>
                             </div>
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5 flex-1">
-                                    <span className="block font-medium text-zinc-100">Reserva do AirBnb</span>
-                                    <a href='#' className="block text-sx text-zinc-400 truncate hover:text-zinc-200">
-                                        https://wwww.airbnb.com.br/rooms/10470001165465465465465132132
-                                    </a>
-                                </div>
-                                <Link2 className="text-zinc-400 size-5"/>
-                            </div>
+                            <Link2 className="text-zinc-400 size-5"/>
                         </div>
-                        <button className='bg-zinc-800 text-zinc-200 w-full rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 justify-center'>
-                            <Plus className='szie-5'/>
-                            Cadastrar novo link
-                        </button>
-                    </div>
-                    <div className='w-full h-px bg-zinc-800'/>
-                    <div className="space-y-6">
-                        <h2 className="font-semibold text-xl">Convidados</h2>
-                        <div className="space-y-5">
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5 flex-1">
-                                    <span className="block font-medium text-zinc-100">Jéssica White</span>
-                                    <span className="block text-sm text-zinc-400 truncate">
-                                        jessica.white@gmail.com.br
-                                    </span>
-                                </div>
-                                <CircleDashed className="text-zinc-400 size-5"/>
+                        <div className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">Reserva do AirBnb</span>
+                                <a href='#' className="block text-sx text-zinc-400 truncate hover:text-zinc-200">
+                                    https://wwww.airbnb.com.br/rooms/10470001165465465465465132132
+                                </a>
                             </div>
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5 flex-1">
-                                    <span className="block font-medium text-zinc-100">Dr. Rita Pachoca</span>
-                                    <span className="block text-sm text-zinc-400 truncate">
-                                    rita.pachoca@gmail.com.br
-                                    </span >
-                                </div>
-                                <CircleDashed className="text-zinc-400 size-5"/>
-                            </div>
+                            <Link2 className="text-zinc-400 size-5"/>
                         </div>
-                        <button className='bg-zinc-800 text-zinc-200 w-full rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 justify-center'>
-                            <UserCog className='szie-5'/>
-                            Gerenciar convidados
-                        </button>
                     </div>
+                    <button className='bg-zinc-800 text-zinc-200 w-full rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 justify-center'>
+                        <Plus className='szie-5'/>
+                        Cadastrar novo link
+                    </button>
                 </div>
-            </main>
-        </div>
+                <div className='w-full h-px bg-zinc-800'/>
+                <div className="space-y-6">
+                    <h2 className="font-semibold text-xl">Convidados</h2>
+                    <div className="space-y-5">
+                        <div className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">Jéssica White</span>
+                                <span className="block text-sm text-zinc-400 truncate">
+                                    jessica.white@gmail.com.br
+                                </span>
+                            </div>
+                            <CircleDashed className="text-zinc-400 size-5"/>
+                        </div>
+                        <div className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">Dr. Rita Pachoca</span>
+                                <span className="block text-sm text-zinc-400 truncate">
+                                rita.pachoca@gmail.com.br
+                                </span >
+                            </div>
+                            <CircleDashed className="text-zinc-400 size-5"/>
+                        </div>
+                    </div>
+                    <button className='bg-zinc-800 text-zinc-200 w-full rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 justify-center'>
+                        <UserCog className='szie-5'/>
+                        Gerenciar convidados
+                    </button>
+                </div>
+            </div>
+        </main>
+    </div>
     )
 }  
