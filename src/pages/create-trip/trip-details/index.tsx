f514e6bb0cab4dabc31fdb9e1e345a9ed3ec5f52
Marcos Gabriel <marcos.gabrielr150@gmail.com>
2024-08-12 10:46:20 -0300

    separateGuests

--- src/pages/create-trip/trip-details/index.tsx
+++ src/pages/create-trip/trip-details/index.tsx
@@ -1,7 +1,8 @@
-import { Calendar, CircleCheck, CircleDashed, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./importan-links";
+import { Guests } from "./ghests-activity-modal";
 
 export function TripDetailsPage () {
 
@@ -84,33 +85,7 @@ export function TripDetailsPage () {
                 <ImportantLinks/>
             
             <div className='w-full h-px bg-zinc-800'/>
-                <div className="space-y-6">
-                    <h2 className="font-semibold text-xl">Convidados</h2>
-                    <div className="space-y-5">
-                        <div className="flex items-center justify-between gap-4">
-                            <div className="space-y-1.5 flex-1">
-                                <span className="block font-medium text-zinc-100">Jéssica White</span>
-                                <span className="block text-sm text-zinc-400 truncate">
-                                    jessica.white@gmail.com.br
-                                </span>
-                            </div>
-                            <CircleDashed className="text-zinc-400 size-5"/>
-                        </div>
-                        <div className="flex items-center justify-between gap-4">
-                            <div className="space-y-1.5 flex-1">
-                                <span className="block font-medium text-zinc-100">Dr. Rita Pachoca</span>
-                                <span className="block text-sm text-zinc-400 truncate">
-                                rita.pachoca@gmail.com.br
-                                </span >
-                            </div>
-                            <CircleDashed className="text-zinc-400 size-5"/>
-                        </div>
-                    </div>
-                    <button className='bg-zinc-800 text-zinc-200 w-full rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700 justify-center'>
-                        <UserCog className='szie-5'/>
-                        Gerenciar convidados
-                    </button>
-                </div>
+                <Guests/>
             </div>
         </main>
 
