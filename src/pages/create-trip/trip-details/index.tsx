d9ea1257a54608f99b4e4e358d64065f3eee5610
Marcos Gabriel <marcos.gabrielr150@gmail.com>
2024-08-12 10:59:50 -0300

    separateDestinationAndDateHeader

--- src/pages/create-trip/trip-details/index.tsx
+++ src/pages/create-trip/trip-details/index.tsx
@@ -1,9 +1,10 @@
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Plus} from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./importan-links";
 import { Guests } from "./ghests-activity-modal";
 import { Activities } from "./activities";
+import { DestinationAndDateHeader } from "./destination-and-date-header";
 
 export function TripDetailsPage () {
 
@@ -19,25 +20,9 @@ export function TripDetailsPage () {
 
     return (
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-            <div className="px-4 h-16 rounded-xl bg-zinc-900 flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <MapPin className="size-5 text-zinc-400"/>
-                   <span className=" text-zinc-100"> Florianópolis, Brasil </span>
-                </div>
 
-                <div className="flex items-center gap-5">
-                <div className="flex items-center gap-2">
-                    <Calendar className="size-5 text-zinc-400"/>
-                   <span className=" text-zinc-100"> 17 a 23 de Agosto </span>
-                </div>
+          <DestinationAndDateHeader/>
 
-                <div className='w-px h-6 bg-zinc-800' />
-                <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-                    Alterar Local/data
-                    <Settings2 className='szie-5'/>
-                </button>
-                </div>
-            </div>
             <main className="flex gap-16 px-4">
                 <div className="flex-1 space-y-6">
                     <div className="flex items-center justify-between">
@@ -46,11 +31,8 @@ export function TripDetailsPage () {
                                 <Plus className='size- 5' />
                                 Cadastrar atividade
                             </button> 
-                        
-            <Activities/>
-        </div>
-
-                    
+                     <Activities/>
+                  </div>
             </div>
 
             <div className="w-80 space-y-6">
